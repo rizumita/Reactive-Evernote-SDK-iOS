31ec2ea8df60ae5b55883023da7c6643a125ed8e
Ryoichi Izumita <r.izumita@caph.jp>
2013-04-23 05:57:14 +0900

    Initial Commit

---
+++ Reactive-Evernote-SDK-iOSTests/Reactive_Evernote_SDK_iOSTests.h
@@ -0,0 +1,13 @@
+//
+//  Reactive_Evernote_SDK_iOSTests.h
+//  Reactive-Evernote-SDK-iOSTests
+//
+//  Created by 和泉田 領一 on 2013/04/23.
+//  Copyright (c) 2013年 CAPH. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Reactive_Evernote_SDK_iOSTests : SenTestCase
+
+@end
---
+++ Reactive-Evernote-SDK-iOSTests/Reactive_Evernote_SDK_iOSTests.m
@@ -0,0 +1,32 @@
+//
+//  Reactive_Evernote_SDK_iOSTests.m
+//  Reactive-Evernote-SDK-iOSTests
+//
+//  Created by 和泉田 領一 on 2013/04/23.
+//  Copyright (c) 2013年 CAPH. All rights reserved.
+//
+
+#import "Reactive_Evernote_SDK_iOSTests.h"
+
+@implementation Reactive_Evernote_SDK_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Reactive-Evernote-SDK-iOSTests");
+}
+
+@end
